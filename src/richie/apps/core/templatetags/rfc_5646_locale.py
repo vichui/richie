71c4f4cdc2a69339e215cd5c9e507db7c9407461
vichui <33508263+vichui@users.noreply.github.com>
2023-07-25 14:18:19 +0800

    Update rfc_5646_locale.py

--- src/richie/apps/core/templatetags/rfc_5646_locale.py
+++ src/richie/apps/core/templatetags/rfc_5646_locale.py
@@ -18,6 +18,7 @@ def rfc_5646_locale(value):
         value_locale = value[:2].lower() + "-" + value[3:].upper()  # noqa
     else:
         value_locale = value.lower()
+    print (value_locale)
     # pylint: disable=unsupported-membership-test
     if value_locale in RFC_5646_LOCALES:
         return value_locale
